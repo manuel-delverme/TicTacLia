c1661c8f9af3df06218027fac22370104283cca0
Manuel <manuel.delverme@gmail.com>
2018-09-05 22:41:25 +0200

    added multiple games

--- playGame.py
+++ playGame.py
@@ -29,40 +29,44 @@ board = [
 ]
 # import numpy as np
 
-turn = random.randint(0, 2)
-print("======= GAME STARTED =======")
-for _ in range(9):
-    print('turn', turn)
-    if turn % 2 == 0:
-        row, column = lia_s_agent(board)
-        print('Lia picked', row, column)
-        
-        if board[row][column] != ' ':
-            print('Lia cheated!')
-            break
-        board[row][column] = 'O'
-        
-    elif turn % 2 == 1:
-        row, column = manu_s_agent(board)
-        print('Manu picked', row, column)
-        if board[row][column] != ' ':
-            print('Manu cheated!')
-            break
-        board[row][column] = 'X'
-    
-    # end of the turn
-    
-    winner = check_winner(board)
-    if winner == 'X':
-        print("Manu wins a kiss from Lia")
-    elif winner == 'O':
-        print("Lia wins ten kisses from Manu")
-    else:
-        print("nobody won yet!")
+nr_games_to_play = 3
+for game_nr in range(nr_games_to_play):
+    print("======= GAME nr {} STARTED =======".format(game_nr))
+    turn = random.randint(0, 2)
+    while True:
+        print('turn', turn)
+        if turn % 2 == 0:
+            row, column = lia_s_agent(board)
+            print('Lia picked', row, column)
 
-    
-    turn += 1
-    
-    for row in board:
-        print(row)
-    print()
+            if board[row][column] != ' ':
+                print('Lia cheated!')
+                break
+            board[row][column] = 'O'
+
+        elif turn % 2 == 1:
+            row, column = manu_s_agent(board)
+            print('Manu picked', row, column)
+            if board[row][column] != ' ':
+                print('Manu cheated!')
+                break
+            board[row][column] = 'X'
+
+        # end of the turn
+
+        winner = check_winner(board)
+        if winner == 'X':
+            print("Manu wins a kiss from Lia")
+            break
+        elif winner == 'O':
+            print("Lia wins ten kisses from Manu")
+            break
+        else:
+            print("nobody won yet!")
+
+
+        turn += 1
+
+        for row in board:
+            print(row)
+        print()
