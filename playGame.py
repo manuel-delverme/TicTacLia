dfe956a78a8ea4664a6c0f200f4c40b97e34ce86
Ionelia Buzatu <buzatuionelia@gmail.com>
2018-09-21 05:48:04 +0200

    dunno what

--- playGame.py
+++ playGame.py
@@ -9,18 +9,91 @@ def manu_s_agent(board):
     return row, column
 
 
+def manu_input(board):
+    print("Please chose a row index, between 0 and 2:")
+    row = input() # must be 0, 1 or 2
+    print("Please chose a column index, between 0 and 2:")
+    column = input() # must be 0, 1, or 2
+    if board[row][column] != ' ':
+        return manu_input(board)
+    return row, column
+
 def lia_s_agent(board):
 
-    row = random.randint(0, 2)
-    column = random.randint(0, 2)
-    if board[1][1] == ' ':
-        row = 1
-        column = 1
-        return row, column
-    if board[row][column] != ' ':
-        return lia_s_agent(board)
+    # row = random.randint(0, 2)
+    # column = random.randint(0, 2)
+    count_freeSpace = 0
+    for rowLoop in range(len(board)):
+        for columnLoop in range(len(board[rowLoop])):
+            if board[rowLoop][columnLoop] == ' ':
+                count_freeSpace += 1
+                if count_freeSpace == 9:
+                    row = 1
+                    column = 1
+                    return row, column
+                if count_freeSpace == 8:
+                    if board[0][1] == 'X' or board[1][0] == 'X' or board[1][2] == 'X' or board[2][1] == 'X':
+                        row = 1
+                        column = 1
+                        return row, column
+                    if board[1][1] == 'X':
+                        row = 0
+                        column = 0
+                        return row, column
+                if count_freeSpace == 7:
+                    if board[1][1] == '0':
+                        if board[0][1] == 'X' or board[1][0] == 'X' or board[1][2] == 'X' or board[2][1] == 'X':
+                            row = 0
+                            column = 0
+                            return row, column
+                        else:
+                            row = 1
+                            column = random.choice([0, 2])
+                            return row, column
+                    if board[1][1] == 'X':
+                        if board[0][0] != ' ':
+                            row = 0
+                            column = 1
+                            return row, column
+                        else:
+                            if board[0][2] != ' ':
+                                row = 0
+                                column = 1
+                                return row, column
+                            else:
+                                if board[2][2] != ' ':
+                                    row = 2
+                                    column = 1
+                                    return row, column
+                                else:
+                                    if board[2][0] != ' ':
+                                        row = 1
+                                        column = 0
+                                        return row, column
+                if count_freeSpace <= 6:
+                    row = random.randint(0, 2)
+                    column = random.randint(0, 2)
+                    return row, column
 
-    return row, column
+                # 6 free spaces left
+                # if count_freeSpace == 6:
+                #
+                # if count_freeSpace == 5:
+                #
+                # if count_freeSpace == 4:
+                #
+                # if count_freeSpace == 3:
+                #
+                # if count_freeSpace == 2:
+                #
+                # if count_freeSpace == 1:
+
+            # game ends
+
+    # if board[row][column] != ' ':
+    #     return lia_s_agent(board)
+    #
+    # return row, column
 
 
 
@@ -88,9 +161,9 @@ for game_nr in range(nr_games_to_play):
             if game_nr < 100:
                 print('Lia picked',  row, column)
 
-            if board[row][column] != ' ':
-                print('Lia cheated!')
-                break
+            # if board[row][column] != ' ':
+            #     print('Lia cheated!')
+            #     break
             board[row][column] = 'O'
             who_plays = 1
 
@@ -100,9 +173,9 @@ for game_nr in range(nr_games_to_play):
                 print('Manu picked', row, column)
 
             # this check if a new place is being overwritten
-            if board[row][column] != ' ':
-                print('Manu cheated!')
-                break
+            # if board[row][column] != ' ':
+            #     print('Manu cheated!')
+            #     break
             board[row][column] = 'X'
             who_plays = 0
 
