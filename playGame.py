89541d1169b9a5c9ef21d06b5e38171f9f76b9da
Ionelia Buzatu <buzatuionelia@gmail.com>
2018-10-01 14:17:07 +0200

    Merge branch 'master' of https://github.com/manuel-delverme/TicTacLia
    # Conflicts:
    #	playGame.py

--- playGame.py
+++ playGame.py
@@ -4,8 +4,10 @@ import argparse
 import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 # import pandas as pd
 import os.path
+
 
 def pick_random_move(board):
     row = random.randint(0, 2)
@@ -44,83 +46,66 @@ def manu_input(board):
         return manu_input(board)
     return row, column
 
-def lia_s_agent(board):
-    count_freeSpace = 0
-    for rowLoop in range(len(board)):
-        for columnLoop in range(len(board[rowLoop])):
-            if board[rowLoop][columnLoop] == ' ':
-                count_freeSpace += 1
 
-    if count_freeSpace == 9:
+def opponent(player):
+    if player == 'X':
+        return 'O'
+    return 'X'
+
+
+def minimax(board, empty_cells, player):  # minimax(state, actions, player = 'O')
+    winner = check_winner(board)
+
+    if winner != False:
+        return (None, None), winner
+
+    else:
+        for emptyCell in empty_cells:
+            newBoard = deepcopy(board)
+            row = emptyCell[0]
+            column = emptyCell[1]
+            newBoard[row][column] = player # insert 'O' in emptyCell
+
+            # updates the empty_cells list and calls the function itself with the board as the newBoard
+            newEmptyCells = findEmptyCells(newBoard)
+
+            move, winner = minimax(newBoard, newEmptyCells, opponent(player))
+
+            # how do I insert Manu's moves?
+
+            # output: who is going to win?
+            # ----> 'O', 'X', DRAW
+            if winner == 'O':
+                # print("The winner is 'O'")
+                return (row, column), winner
+
+            if winner == 'X':
+                pass
+        # in case it's a draw
+        output = pick_random_move(board)
+        return output, winner
+
+
+def lia_s_agent(board):
+    empty_cells = findEmptyCells(board)
+
+    if len(empty_cells) == 9:
         row = 1
         column = 1
         return row, column
 
-    if count_freeSpace == 8:
-        if board[0][1] == 'X' or board[1][0] == 'X' or board[1][2] == 'X' or board[2][1] == 'X':
-            row = 1
-            column = 1
-            return row, column
-        if board[1][1] == 'X':
-            row = 0
-            column = 0
-            return row, column
+    else:
+        output = minimax(board, empty_cells, player = 'O')
+        return output[0]
 
-    if count_freeSpace == 7:
-        if board[1][1] == 'O':
-            if board[0][1] == 'X' or board[1][0] == 'X' or board[1][2] == 'X' or board[2][1] == 'X':
-                row = 0
-                column = 0
-                return row, column
-            else:
-                row = 1
-                column = random.choice([0, 2])
-                return row, column
-        if board[1][1] == 'X':
-            if board[0][0] != ' ':
-                row = 0
-                column = 1
-                return row, column
-            else:
-                if board[0][2] != ' ':
-                    row = 0
-                    column = 1
-                    return row, column
-                else:
-                    if board[2][2] != ' ':
-                        row = 2
-                        column = 1
-                        return row, column
-                    else:
-                        if board[2][0] != ' ':
-                            row = 1
-                            column = 0
-                            return row, column
 
-    if count_freeSpace <= 6:
-        return pick_random_move(board)
-    return pick_random_move(board)
-
-                # 6 free spaces left
-                # if count_freeSpace == 6:
-                #
-                # if count_freeSpace == 5:
-                #
-                # if count_freeSpace == 4:
-                #
-                # if count_freeSpace == 3:
-                #
-                # if count_freeSpace == 2:
-                #
-                # if count_freeSpace == 1:
-
-            # game ends
-
-    # if board[row][column] != ' ':
-    #     return lia_s_agent(board)
-    #
-    # return row, column
-
+def findEmptyCells(board):
+    empty_cells = []  # count_freeSpace or depth
+    for rowLoop in range(len(board)):
+        for columnLoop in range(len(board[rowLoop])):
+            if board[rowLoop][columnLoop] == ' ':
+                empty_cells.append([rowLoop, columnLoop])
+    return empty_cells
 
 
 def check_winner(board):
@@ -159,22 +144,27 @@ def check_winner(board):
         if board[0][2] == "O":
             if (board[1][1] == "O") and (board[2][0] == "O"):
                 return "O"
+
+        checkBoard = findEmptyCells(board)
+        if len(checkBoard) == 0:
+            return "It's a Draw"
+
         return False
 
 
 def main(args):
     variances = []
-    nr_games_to_play = [10, 100, 1000, 10000]
+    nr_games_to_play = [10, 100, 1000]
     for game_size in nr_games_to_play:
         samples = []
-        gameRatio = play_games(args, game_size, 0)
         for sample in range(5):
             varRatio = play_games(args, game_size, 0)
             samples.append(varRatio)
         variance = np.var(samples)
         variances.append(variance)
+        mean = np.mean(samples)
 
-        print("The variance of {} games size is: {}".format (game_size, variance))
+        print("The mean is {} and the variance of {} games size is: {}".format (mean, game_size, variance))
     plt.loglog(nr_games_to_play, variances)
     plt.title("manu_is_drunk={}".format(args.manu_is_drunk))
     try:
@@ -253,12 +243,11 @@ def play_games(args, nr_games_to_play, gameShow):
                 leaderboard['Lia'] += 1
                 break
             # check if game is draw, and exit loop
-            else:
-                if turn == 9:
-                    if game_nr < gameShow:
-                        print("It's a Draw")
-                    leaderboard['Draw'] += 1
-                    break
+            elif winner == "It's a Draw":
+                if game_nr < gameShow:
+                    print("It's a Draw")
+                leaderboard['Draw'] += 1
+                break
 
     total_wins = float(leaderboard['Lia'] + leaderboard['Manu'])
     winningRatio = float(leaderboard['Lia'])/total_wins
